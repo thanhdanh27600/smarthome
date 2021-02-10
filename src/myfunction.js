adf974239f4df28256c16abea43f83a4c3c4cee7
thanhdanh27600 <47974698+thanhdanh27600@users.noreply.github.com>
2021-02-10 20:39:34 +0700

    updated

--- src/myfunction.js
+++ src/myfunction.js
@@ -80,7 +80,7 @@ function dht() {
             else { 
                 var obj = JSON.parse(response);
                 document.getElementById("real_temp").innerText = obj.hic;
-                document.getElementById("temparature").innerText = " : " + obj.temp;
+                document.getElementById("temparature").innerText = " : " + obj.temp + 'Degree Celsisus';
                 document.getElementById("relative_humid").innerText = " : " + obj.humid +"%";
             }
         }
