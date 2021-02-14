7a8ea357c18a01f0d5319f830e45a688a0e67c17
thanhdanh27600 <47974698+thanhdanh27600@users.noreply.github.com>
2021-02-14 15:27:59 +0700

    Update myfunction.js

--- src/myfunction.js
+++ src/myfunction.js
@@ -87,7 +87,7 @@ function dht() {
                 alert(obj.temp);
                 document.getElementById("relative_humid").innerText = +obj.humid + "%";
                 var str = document.getElementById("temparature").innerText
-                document.getElementById("temparature").innerText = str.substring(0, str.length - 2) + str.charAt(str.length - 1);
+                //document.getElementById("temparature").innerText = str.substring(0, str.length - 2) + str.charAt(str.length - 1);
             }
         }
     })
